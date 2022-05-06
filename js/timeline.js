b79a3d5e3bc5ec2c4296ca4baff626b5b960bf60
sndmcglnchy <sineadmcglinchey18@gmail.com>
2022-05-06 21:41:34 +0100

    Try out motion path plugin

--- js/timeline.js
+++ js/timeline.js
@@ -423,3 +423,21 @@ slider.addEventListener('mousedown', function(){
 slider.addEventListener('mouseup', function(){
   slider.style.cursor = "grab";
 })
+
+
+
+gsap.registerPlugin(MotionPathPlugin);
+
+gsap.to("#rect", {
+  duration: 5, 
+  repeat: 12,
+  repeatDelay: 3,
+  yoyo: true,
+  ease: "power1.inOut",
+  motionPath:{
+    path: "#path",
+    align: "#path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5]
+  }
+});
